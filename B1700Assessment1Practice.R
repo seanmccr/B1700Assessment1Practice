01ad3347fcd225823444bbe7c5af943ca1147f57
seanmccr <seanmccrone1@gmail.com>
2023-10-08 15:35:54 +0100

    Assessment 1 Practice
    First save

---
+++ B1700Assessment1Practice.R
@@ -0,0 +1,30 @@
+B1700 Assessment 1 - Practice 
+
+Tasks to be Completed:
+  - Loading libraries required for script to run 
+  - Importing data from different file formations, inc. .csv and .xls
+  - Creating dataframe from imported file
+  - Creating a markdown document in R
+  - Make sure code is commented approriately
+  - Renaming variables in the dataset
+  - Changing data types in R (ex: character to integar)
+  - Creating a new variable based on an operation carried out using existing variables
+  - Dealing with missing data by removing rows that contain missing data
+  - Removing specific variables from the dataset
+  - Identifying outliers in the dataset, and dealing with these appropriately
+  - Calculating & printing descriptive statistics (ex: mean, median, min, max, SD)
+  - Calculating histograms for specific variables
+  - Creating boxplot for specific variables & being able to create boxplots of different groups 
+  - Creating a corrolation matrix of variables in the dataset, and creating a heatmap of this matrix
+  - Creating a scatter plot of two variables in the dataset & adding linear regression line to scatterplot
+
+To finish, create a R Markdown file (which will be requested in the assessment) to send away as a
+matter of proper process
+-------------------------------------------------------------------------
+  
+1) Loading libraries required for script to run:
+  
+  
+  
+  
+  
